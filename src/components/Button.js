3de71f592815d1da6d5b646c189ef7b06a9ae1a6
Sona Varghese <sonasusan.v@gmail.com>
2020-12-07 20:45:15 -0500

    Commit changes to Button and scss changes for different button states

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,17 @@
 import React from "react";
-
 import "components/Button.scss";
 
+const classnames = require ('classnames');
+
 export default function Button(props) {
+   const buttonClass = classnames("button", {
+      "button--confirm" : props.confirm,
+      "button--danger" : props.danger
+   });
+
    return (
-      <button>Submit</button>
+      <button className={buttonClass} onClick={props.onClick} disabled={props.disabled}>
+         {props.children}
+      </button>
    );
 }
