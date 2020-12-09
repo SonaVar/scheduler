f7e13d4bf1f7c1e4549adb294462dd1f2f2b8c78
Sona Varghese <sonasusan.v@gmail.com>
2020-12-08 19:05:11 -0500

    Commit InterviewerList component with scss

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import "components/InterviewerList.scss"
+
+import InterviewerListItem from './InterviewerListItem';
+
+export default function InterviewerList(props) {
+  const interviewers = props.interviewers.map(item => {
+    return (
+      <InterviewerListItem 
+        key={item.id}
+        name={item.name}
+        avatar={item.avatar}
+        selected={item.id === props.interviewer}
+        setInterviewer={event => props.setInterviewer(item.id)}
+      />
+    );
+  })
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {interviewers}
+      </ul>
+    </section>
+  );
+}
