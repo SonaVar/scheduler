5f7a2f04d4b8f0921a8a87f61c6d84b4b2247c46
Sona Varghese <sonasusan.v@gmail.com>
2020-12-09 16:50:25 -0500

    Commit changes to seeding data from API and using axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,29 +1,96 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import DayList from "components/DayList";
+import "components/Appointment"
 
 import "components/Application.scss";
+import Appointment from "components/Appointment";
 
-const days = [
+const axios = require('axios');
+
+const appointments = [
   {
     id: 1,
-    name: "Monday",
-    spots: 2,
+    time: "12pm",
   },
   {
     id: 2,
-    name: "Tuesday",
-    spots: 5,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
   },
   {
     id: 3,
-    name: "Wednesday",
-    spots: 0,
+    time: "1pm",
+    interview: {
+      student: "Antony Palmer",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      }
+    }
   },
+  {
+    id: 4,
+    time: "2pm",
+    interview: {
+      student: "Mercy Linguini",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "3pm",
+  },
+  {
+    id: 6,
+    time: "4pm",
+    interview: {
+      student: "Gloria McMuffin",
+      interviewer: {
+        id: 2,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: "last",
+    time: "5pm"
+  }
 ];
 
 export default function Application(props) {
+  const [days, setDays] = useState([]);
   const [day, setDate] = useState("Monday");
+  const schedule = appointments.map(item => {
+    return (
+      <Appointment
+        key={item.id}
+        {...item}
+      />
+    );
+  });
+
+  useEffect(() => {
+    const url = '/api/days'
+    axios.get(url).then(response => {
+      console.log(response)
+      setDays([...response.data])
+    })
+  }, [])
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -31,6 +98,7 @@ export default function Application(props) {
       <hr className="sidebar__separator sidebar--centered" />
       <nav className="sidebar__menu">
         <DayList
+          key={days.id}
           days={days}
           day={day}
           setDay={setDate}
@@ -39,7 +107,9 @@ export default function Application(props) {
       <img className="sidebar__lhl sidebar--centered" src="images/lhl.png" alt="Lighthouse Labs"/>
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        <ul>
+          {schedule}
+        </ul>
       </section>
     </main>
   );
